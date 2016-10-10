772d78c5f4eea3e2b767039f81a33814e6b5eb62
Rustem Kakimov <ruskakimov@gmail.com>
2016-10-10 11:26:45 +0800

    added socket.io broadcasting

--- public/sketch.js
+++ public/sketch.js
@@ -2,31 +2,71 @@ APP = {};
 APP.canvas = document.getElementById('board');
 APP.ctx = APP.canvas.getContext('2d');
 
-APP.canvas.width = 500;
-APP.canvas.height = 500;
-APP.tool = new Pencil();
+APP.setup = function () {
+  APP.canvas.width = 500;
+  APP.canvas.height = 500;
+  APP.tool = new Pencil();
+  APP.socket = io.connect('http://localhost:8080');
+
+  APP.socket.on('start', function (data) {
+    console.log("Started path at: " + data.x + " " + data.y);
+    APP.tool.mousedown(data, false);
+  });
+
+  APP.socket.on('draw', function (data) {
+    console.log("Drawing at: " + data.x + " " + data.y);
+    APP.tool.mousemove(data, false);
+  });
+
+  APP.socket.on('end', function (data) {
+    console.log("Ended path at: " + data.x + " " + data.y);
+    APP.tool.mouseup(data, false);
+  });
+}
 
 function Pencil (e) {
   this.drawing = false;
-  this.color = "#55F";
+  this.clientColor = "#55F";
+  this.secondColor = "#F55";
   this.width = 5;
+  APP.ctx.lineCap = 'round';
+  APP.ctx.lineJoin = 'round';
 
-  this.mousedown = function (e, pos) {
+  this.mousedown = function (pos, client) {
     this.drawing = true;
-    APP.ctx.beginPath();
-    APP.ctx.strokeStyle = this.color;
+    if (client) {
+      APP.ctx.strokeStyle = this.clientColor;
+      APP.socket.emit('startpath', pos);
+    } else {
+      APP.ctx.strokeStyle = this.secondColor;
+    }
     APP.ctx.lineWidth = this.width;
+    APP.ctx.beginPath();
     APP.ctx.moveTo(pos.x, pos.y)
   };
-  this.mousemove = function (e, pos) {
+  this.mousemove = function (pos, client) {
     if (this.drawing) {
       APP.ctx.lineTo(pos.x, pos.y);
       APP.ctx.stroke();
+      if (client) {
+        APP.socket.emit('drawing', pos);
+      }
     }
   };
-  this.mouseup = function (e, pos) {
+  this.mouseup = function (pos, client) {
     if (this.drawing) {
       this.drawing = false;
+      if (client) {
+        APP.socket.emit('endpath', pos);
+      }
+    }
+  };
+  this.mouseout = function (pos, client) {
+    if (this.drawing) {
+      this.drawing = false;
+      if (client) {
+        APP.socket.emit('endpath', pos);
+      }
     }
   };
 }
@@ -42,12 +82,14 @@ APP.getMousePosition = function (canvas, e) {
 APP.eventHandler = function (e) {
   var pos = APP.getMousePosition(this.canvas, e);
   try {
-    APP.tool[e.type](e, pos);
+    APP.tool[e.type](pos, true);
   } catch (e) {
     console.log(e);
   }
 };
 
+APP.setup();
 APP.canvas.addEventListener('mousedown', APP.eventHandler);
 APP.canvas.addEventListener('mousemove', APP.eventHandler);
 APP.canvas.addEventListener('mouseup', APP.eventHandler);
+APP.canvas.addEventListener('mouseout', APP.eventHandler);
--- server.js
+++ server.js
@@ -1,5 +1,33 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(3001);
+var server = app.listen(8080);
 app.use(express.static('public'));
+
+var io = require('socket.io')(server);
+
+io.sockets.on('connection',
+  function (socket) {
+    console.log("New client: " + socket.id);
+
+    socket.on('startpath', function (data) {
+        console.log("Started drawing at :" + data.x + " " + data.y);
+        socket.broadcast.emit('start', data);
+    });
+
+    socket.on('drawing', function (data) {
+        console.log("Received drawing at :" + data.x + " " + data.y);
+        socket.broadcast.emit('draw', data);
+      }
+    );
+
+    socket.on('endpath', function (data) {
+        console.log("Ended drawing at :" + data.x + " " + data.y);
+        socket.broadcast.emit('end', data);
+    });
+
+    socket.on('disconnect', function () {
+      console.log(socket.id + " has disconnected.");
+    });
+  }
+);
